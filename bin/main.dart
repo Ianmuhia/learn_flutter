92d43abfb790034f4c9be06e2acced43119b6d24
ian muhia <ianmuhia3@gmail.com>
2020-01-05 23:54:05 +0300

    flutter fuctions

---
+++ bin/main.dart
@@ -0,0 +1,38 @@
+// main(List<String> args) {
+//   doSomething();
+//   showAge();
+//   print(showAge());
+// }
+
+// void doSomething(){
+//   print('hello world!');
+//   sayMyName();
+//   print(sayMyName());
+// }
+
+// String sayMyName(){
+//   var name = 'ian';
+
+//   if (name.contains('i')) {
+//     print('name contains ian');
+//   } else {
+
+//     return null;
+//   }
+
+//   return 'function compiled succesfully';
+
+// }
+
+// String showAge()=> 'johnte kamami';
+ 
+
+ main(List<String> args) {
+   print(sayhello("ian", "kamami", 10)); 
+ }
+
+//  String sayHello(String name){
+//    return "hello $name";
+//  }
+
+String sayhello(String name, String lastname, int age) => "hellllo $name $lastname S$age "; 
