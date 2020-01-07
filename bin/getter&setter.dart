968b941eb27047e5096f197c8eab0de08c3a023e
ian muhia <ianmuhia3@gmail.com>
2020-01-07 23:58:34 +0300

    getters and setters added

---
+++ bin/getter&setter.dart
@@ -0,0 +1,69 @@
+main(List<String> args) {
+  var mic = new Microphone("blue yeti",'silver',20 );
+
+  mic.setName = 'JOHNTE';
+
+  print(mic.getName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // var micSecond = new Microphone.initialize();
+  // print(micSecond.model); //using the named  constructor
+
+  // // print(mic.color);
+  // mic.setVolume();
+  // mic.TurnOff();
+  // mic.TurnOn();
+  // // var modelNumber = mic.modelNumber();
+  // // var on = mic.isOn();
+  // // print(on);
+  // // print(modelNumber);
+}
+
+class Microphone {
+ String name;
+ String color;
+ int model;
+
+
+  Microphone(this.name, this.color, this.model);
+  Microphone.initialize(){
+    name ='blue';
+    model =54;
+  }
+
+
+  String get getName => name;
+  set setName(String value) => name =value;
+
+  void TurnOn(){
+    print("$name is turned  on");
+  }
+
+
+  void TurnOff(){
+    print("$name is turned  off");
+  }
+
+  void setVolume(){
+    print("$name  vokume os up");
+  
+  }
+
+  bool isOn() => true;
+
+  int modelNumber() => model;
+
+}
