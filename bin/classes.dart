0a8a6bf231d5aaf00cde4a28180df8d9f4710e45
ian muhia <ianmuhia3@gmail.com>
2020-01-07 23:45:16 +0300

    classes final

--- bin/classes.dart
+++ bin/classes.dart
@@ -1,12 +1,46 @@
 main(List<String> args) {
-  
+  var mic = new Microphone("blue yeti",'silver',20 );
+  var micSecond = new Microphone.initialize();
+  print(micSecond.model);
+
+  // print(mic.color);
+  mic.setVolume();
+  mic.TurnOff();
+  mic.TurnOn();
+  // var modelNumber = mic.modelNumber();
+  // var on = mic.isOn();
+  // print(on);
+  // print(modelNumber);
 }
 
 class Microphone {
-  String ian;
+ String name;
+ String color;
+ int model;
 
-  Microphone(this.ian);
-  Microphone
 
+  Microphone(this.name, this.color, this.model);
+  Microphone.initialize(){
+    name ='blue';
+    model =54;
+  }
+
+  void TurnOn(){
+    print("$name is turned  on");
+  }
+
+
+  void TurnOff(){
+    print("$name is turned  off");
+  }
+
+  void setVolume(){
+    print("$name  vokume os up");
+  
+  }
+
+  bool isOn() => true;
+
+  int modelNumber() => model;
 
 }
